d669de5b05ffb362319230db8e1b945a2af1df59
Mvargas64 <mvargasbulnes@yahoo.com>
2024-03-04 17:49:38 -0600

    plant 1 layout

--- app/src/main/java/com/example/plantidentificationapp/Plant1.java
+++ app/src/main/java/com/example/plantidentificationapp/Plant1.java
@@ -2,7 +2,10 @@ package com.example.plantidentificationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Plant1 extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class Plant1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant1);
+
     }
+    public void onClickMainBTN(View v){
+        Intent ini = new Intent(Plant1.this, MainActivity.class);
+        startActivity(ini);
+    }
+
 }

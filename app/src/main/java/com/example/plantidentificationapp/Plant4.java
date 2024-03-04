c3af56b4520dd97b32ca860e796b6ef2bfb78003
Nic Black <78877662+NicBB5@users.noreply.github.com>
2024-03-04 16:44:27 -0600

    added plant 4 activity and layout

---
+++ app/src/main/java/com/example/plantidentificationapp/Plant4.java
@@ -0,0 +1,14 @@
+package com.example.plantidentificationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Plant4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant4);
+    }
+}

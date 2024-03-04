e66b78444c2e2ad7d40d4dd12a16ee3c38f71439
Wyatt Morgan <iamtheyy@gmail.com>
2024-03-04 16:50:34 -0600

    Added Button to Plant 2
    Added button to plant 2 and routed the plant 2 button on the main page to go to the plant 2 page.

--- app/src/main/java/com/example/plantidentificationapp/Plant2.java
+++ app/src/main/java/com/example/plantidentificationapp/Plant2.java
@@ -2,7 +2,9 @@ package com.example.plantidentificationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Plant2 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Plant2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant2);
+    }
+
+    public void onClickMainBTN(View v){
+        Intent ini = new Intent(this, MainActivity.class);
+        startActivity(ini);
     }
 }

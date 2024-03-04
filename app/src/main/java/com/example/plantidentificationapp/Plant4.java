cf36db658e27d4e7c3896996228418a34a42f59c
Nic Black <78877662+NicBB5@users.noreply.github.com>
2024-03-04 17:36:45 -0600

    added button and plantlist

--- app/src/main/java/com/example/plantidentificationapp/Plant4.java
+++ app/src/main/java/com/example/plantidentificationapp/Plant4.java
@@ -2,7 +2,9 @@ package com.example.plantidentificationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Plant4 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Plant4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant4);
+    }
+
+    public void onClickMainBTN(View v){
+        Intent ini = new Intent(Plant4.this, MainActivity.class);
+        startActivity(ini);
     }
 }

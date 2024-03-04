7cac273eb99f7f0c75ccb0edbe1b73d7a71f5381
Jakelayman0110 <99349809+Jakelayman0110@users.noreply.github.com>
2024-03-04 17:33:19 -0600

    main activity button functionality

--- app/src/main/java/com/example/plantidentificationapp/MainActivity.java
+++ app/src/main/java/com/example/plantidentificationapp/MainActivity.java
@@ -13,12 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.Plant2BTN);
-        button.setOnClickListener(this::onClickPlant2BTN);
+        Button button1 = findViewById(R.id.Plant2BTN);
+        button1.setOnClickListener(this::onClickPlant2BTN);
+        Button button2 = findViewById(R.id.Plant1BTN);
+        button2.setOnClickListener(this::onClickButton1);
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(this::onClickPlant3BTN);
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(this::onClickPlant4BTN);
     }
 
+    public void onClickButton1(View v){
+        Intent ini = new Intent(MainActivity.this, Plant1.class);
+        startActivity(ini);
+    }
     public void onClickPlant2BTN(View v){
         Intent ini = new Intent(MainActivity.this, Plant2.class);
         startActivity(ini);
     }
+
+    public void onClickPlant3BTN(View v){
+        Intent ini = new Intent(MainActivity.this, Plant3.class);
+        startActivity(ini);
+    }
+
+    public void onClickPlant4BTN(View v){
+        Intent ini = new Intent(MainActivity.this, Plant4.class);
+        startActivity(ini);
+    }
+
 }

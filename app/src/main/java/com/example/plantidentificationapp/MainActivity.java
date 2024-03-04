ebb63b01c0a426d660e3672182008317ffb4501f
Jakelayman0110 <99349809+Jakelayman0110@users.noreply.github.com>
2024-03-04 17:05:09 -0600

    trying to make buttons work

--- app/src/main/java/com/example/plantidentificationapp/MainActivity.java
+++ app/src/main/java/com/example/plantidentificationapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this::onClickPlant2BTN);
     }
 
     public void onClickPlant2BTN(View v){
--- app/src/main/java/com/example/plantidentificationapp/Plant2.java
+++ app/src/main/java/com/example/plantidentificationapp/Plant2.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Plant2 extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class Plant2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant2);
+        Button mainBTN = findViewById(R.id.MainBTN);
+        mainBTN.setOnClickListener(this::onClickMainBTN);
     }
 
     public void onClickMainBTN(View v){

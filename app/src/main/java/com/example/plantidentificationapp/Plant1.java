236830d90d8cacacf3c1bc40255fcc194f8fb763
Mvargas64 <mvargasbulnes@yahoo.com>
2024-03-04 16:33:01 -0600

    plant1 first layout

---
+++ app/src/main/java/com/example/plantidentificationapp/Plant1.java
@@ -0,0 +1,14 @@
+package com.example.plantidentificationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Plant1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant1);
+    }
+}

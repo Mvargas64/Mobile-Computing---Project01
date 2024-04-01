1d1778fde6587d5c84f661f00a2329f851e34bf2
Mvargas64 <mvargasbulnes@yahoo.com>
2024-04-01 11:48:14 -0500

    HELP

--- app/src/main/java/com/example/plantidentificationapp/MainActivity.java
+++ app/src/main/java/com/example/plantidentificationapp/MainActivity.java
@@ -30,14 +30,12 @@ public class MainActivity extends AppCompatActivity {
     public void onClickButton1(View v){
         Intent intent = new Intent(MainActivity.this, PlantList.class);
         intent.putExtra("listType", "Plant1"); // Pass the identifier
-        intent.putExtra("Pepper/Capsicum Annuum", "Chili Pepper");
         startActivity(intent);
     }
 
     public void onClickPlant2BTN(View v){
         Intent intent = new Intent(MainActivity.this, PlantList.class);
         intent.putExtra("listType", "Plant2"); // Pass the identifier
-        intent.putExtra("Pepper/Capsicum Annum", "Jalepeno");
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/plantidentificationapp/PlantList.java
+++ app/src/main/java/com/example/plantidentificationapp/PlantList.java
@@ -1,6 +1,5 @@
 package com.example.plantidentificationapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +17,9 @@ import java.util.List;
 
 public class PlantList extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private PlantAdapter plantAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +28,8 @@ public class PlantList extends AppCompatActivity {
         // Retrieve the list type identifier
         String listType = getIntent().getStringExtra("listType");
 
-        // Set up your RecyclerView based on the listType
-        RecyclerView recycleView = findViewById(R.id.recycleView);
-        recycleView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView = findViewById(R.id.recycleView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         List<PlantModel.Plant> plantList = new ArrayList<>();
 
@@ -36,6 +37,10 @@ public class PlantList extends AppCompatActivity {
         switch (listType) {
             case "Plant1":
                 plantList.add(new PlantModel.Plant("Plant 1 Name", "Scientific Name 1", "Description 1", "Lifespan 1", "Type 1"));
+                plantList.add(new PlantModel.Plant("Plant 2 Name", "Scientific Name 2", "Description 2", "Lifespan 2", "Type 2"));
+                plantList.add(new PlantModel.Plant("Plant 3 Name", "Scientific Name 3", "Description 3", "Lifespan 3", "Type 3"));
+                plantList.add(new PlantModel.Plant("Plant 4 Name", "Scientific Name 4", "Description 4", "Lifespan 4", "Type 4"));
+
                 break;
             case "Plant2":
                 plantList.add(new PlantModel.Plant("Plant 2 Name", "Scientific Name 2", "Description 2", "Lifespan 2", "Type 2"));
@@ -51,8 +56,9 @@ public class PlantList extends AppCompatActivity {
                 break;
         }
 
-        PlantAdapter adapter = new PlantAdapter(plantList);
-        recycleView.setAdapter(adapter);
+        // Initialize and set up adapter
+        plantAdapter = new PlantAdapter(plantList);
+        recyclerView.setAdapter(plantAdapter);
     }
 
     public void onClickMainBTN(View v) {
@@ -68,30 +74,26 @@ public class PlantList extends AppCompatActivity {
             this.plantList = plantList;
         }
 
-        @NonNull
         @Override
-        public PlantViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        public PlantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_plant1, parent, false);
             return new PlantViewHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull PlantViewHolder holder, int position) {
-            final PlantModel.Plant plant = plantList.get(position); // Declare music as final here
+        public void onBindViewHolder(PlantViewHolder holder, int position) {
+            final PlantModel.Plant plant = plantList.get(position);
             holder.nameTextView.setText(plant.getName());
-            //holder.artistTextView.setText(plant.get());
 
+            // Set onClickListener for the "play" button
             holder.playButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(PlantList.this, MainActivity.class);
-                    // Pass the song name as an extra
-                    intent.putExtra("songName", plant.getName());
-                    startActivity(intent);
+                    Intent integer = new Intent(PlantList.this, PlantDetailsActivity.class);
+                    startActivity(integer);
                 }
             });
         }
-
 
         @Override
         public int getItemCount() {
@@ -99,16 +101,14 @@ public class PlantList extends AppCompatActivity {
         }
 
         class PlantViewHolder extends RecyclerView.ViewHolder {
-
             TextView nameTextView;
             Button playButton;
 
-            public PlantViewHolder(@NonNull View itemView) {
+            public PlantViewHolder(View itemView) {
                 super(itemView);
                 nameTextView = itemView.findViewById(R.id.nameTV);
-                //artistTextView = itemView.findViewById(R.id.artistTV);
                 playButton = itemView.findViewById(R.id.playBTN);
             }
         }
     }
-}
+    }
